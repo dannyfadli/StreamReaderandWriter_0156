df455584be608aee218118904232d89267a62b26
dannyfadli <danyfdly95@gmail.com>
2024-06-04 10:40:30 +0700

    membuat proyek baru dimana menambahkan path

---
+++ iofilepakaipath/iofilepakaipath.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama file : ";
+    cin >> NamaFile;
+
+    //Membuka file dalam mode menulis.
+    ofstream outfile;
+    //Menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //Unlimeted loop untuk menulis
+    while (true)
+    {
+        cout << "-";
+        //Mendapatkan setiap karakter dalam satu baris  
+        getline(cin, baris);
+        //Loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //Menulis dan memasukkan nilai dari 'baris' ke dalam file
+
+    }
+}
+
