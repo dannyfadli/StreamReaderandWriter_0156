ad06d2eaa2847d7ebfac2b6ce033534253cd64be
dannyfadli <danyfdly95@gmail.com>
2024-06-04 10:14:04 +0700

    menambahkan loop akan berhenti jika anda memasukkan karakter q

--- StreamReaderandWriter_0156/StreamReaderandWriter_0156.cpp
+++ StreamReaderandWriter_0156/StreamReaderandWriter_0156.cpp
@@ -24,7 +24,7 @@ int main()
         //Mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
         //loop akan berhenti jika anda memasukkan karakter q
-
+        if (baris == "q")break;
     }
 
 }
